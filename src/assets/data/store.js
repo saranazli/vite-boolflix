5db31a8c9470d4f60eab3e06822090405fde269b
s4r44d43 <sararez@live.it>
2024-04-04 15:41:06 +0200

    [add] poster to cards

--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -8,10 +8,11 @@ export const store = reactive ({
   // per il metodo di get api con type
   // searchingMovie: '',
   apiUrl: 'https://api.themoviedb.org/3/search/',
+  imgUrlSize: 'http://image.tmdb.org/t/p/w300',
   apiParams: {
     api_key: '143eeaa2ea390065982ac4cac590ae6c',
     query: '',
-    original_language: 'it-IT'
+    original_language: 'it-IT',
   },
   movie: [],
   tv: [],
--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -21,10 +21,12 @@
   <div class="col mb-3">
     <div class="mt-3">
       <div class="card-body">
+        <img :src="store.imgUrlSize + cardObj.poster_path" alt="poster">
         <h5 class="card-title">{{ cardObj.title || cardObj.name }}</h5>
         <h6 class="card-title">{{ cardObj.original_title || cardObj.original_name }}</h6>
-        <img :src="`${store.imgEng}`" alt="en">
-        <p class="text-center">{{ cardObj.original_language  }}</p>
+        <img class="flag" v-if="cardObj.original_language === 'en' " :src="store.imgEng" alt="en">
+        <img class="flag" v-else-if="cardObj.original_language === 'it' " :src="store.imgIt" alt="it">
+        <p class="text-center" v-else>{{ cardObj.original_language }}</p>
         <p class="card-title">{{ cardObj.vote_average }}</p>
       </div>
     </div>
@@ -32,5 +34,7 @@
 </template>
 
 <style lang="scss" scoped>
-
+  img.flag{
+    max-width: 25px;
+  }
 </style>
